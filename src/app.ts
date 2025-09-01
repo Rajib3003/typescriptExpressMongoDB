2216fa64e4cdc6a0ccbd83fdbc8e1d5c2f39f9ab
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-09-01 12:46:35 +0600

    vercel methods add

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,28 @@
-import express , { Application, Request, Response }  from 'express';
-
+import express, { Application, Request, Response } from 'express';
 import { booksRouters } from "./app/controllers/books.controllers";
 import { borrowRouters } from './app/controllers/borrow.controllers';
 import cors from 'cors';
 
+const app: Application = express();
 
-const app :Application = express();
 app.use(express.json());
 
 app.use(
   cors({
-    origin: ['https://b5a4-react-redux.vercel.app/','http://localhost:5173']
-   })
+    origin: [
+      'https://b5a4-react-redux.vercel.app', // ✅ এখানে শেষের স্ল্যাশ তুলে দাও
+      'http://localhost:5173'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true
+  })
 );
-// localhost add korlam
 
-app.use('/api/books', booksRouters)
-app.use('/api/borrow', borrowRouters)
+app.use('/api/books', booksRouters);
+app.use('/api/borrow', borrowRouters);
 
-app.get("/", (req : Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Express server start!!!!");
 });
-
-
 
 export default app;
