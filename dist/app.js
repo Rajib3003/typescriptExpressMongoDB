19db8978e87b04b74b429c2bd33a0781c77bdd57
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-09-02 11:51:04 +0600

    some code remove app.ts

--- dist/app.js
+++ dist/app.js
@@ -9,17 +9,6 @@ const borrow_controllers_1 = require("./app/controllers/borrow.controllers");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-// এখানে বসাও
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://b5a4-react-redux.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
 const allowedOrigins = [
     "http://localhost:5173",
     "https://b5a4-react-redux.vercel.app"
